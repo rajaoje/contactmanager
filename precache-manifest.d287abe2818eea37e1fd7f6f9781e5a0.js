c3b3b3bd24756a655564219f4f7bef3e84a493f8
unknown <rajaoje@gmail.com>
2018-10-30 08:35:53 -0400

    Updates

---
+++ precache-manifest.d287abe2818eea37e1fd7f6f9781e5a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "84e16757db2d1391948a",
+    "url": "/contactmanager/static/js/main.84e16757.chunk.js"
+  },
+  {
+    "revision": "370214697a85ee8ccecc",
+    "url": "/contactmanager/static/js/1.37021469.chunk.js"
+  },
+  {
+    "revision": "84e16757db2d1391948a",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "370214697a85ee8ccecc",
+    "url": "/contactmanager/static/css/1.b7db414e.chunk.css"
+  },
+  {
+    "revision": "e0da13ee85d728cece324305776bad5b",
+    "url": "/contactmanager/index.html"
+  }
+];
